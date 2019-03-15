6b100f77b50bac9612f7cde8223747deaaaa5b1a
d3ah <diegoeauyonh@gmail.com>
2019-03-15 11:27:13 -0600

    Adding auth urls and django icons.

--- ronald/ronald/settings.py
+++ ronald/ronald/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
@@ -40,6 +39,8 @@ INSTALLED_APPS = [
     # Scss and css
     'bootstrap4',
     'sass_processor',
+    # Font awesome
+    'django_icons',
     # Internal
     'host.apps.HostConfig',
 ]
@@ -83,7 +84,7 @@ ROOT_URLCONF = 'ronald.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'ronald/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -128,6 +129,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
